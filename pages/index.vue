f71ac362eb98ad452fc1c9232e3dcf0d69b24598
Suvradip Saha <Suvradip@fusioncharts.com>
2020-03-17 18:25:48 +0530

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+   <div class="container">
+      <div>
+         <h1 class="title">
+            kudos
+         </h1>
+      </div>
+   </div>
+</template>
+
+<script>
+export default {
+   components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+   margin: 0 auto;
+   min-height: 100vh;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   text-align: center;
+
+   .title {
+      font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+         'Helvetica Neue', Arial, sans-serif;
+      display: block;
+      font-weight: 300;
+      font-size: 100px;
+      color: #f54923;
+      letter-spacing: 1px;
+   }
+
+   .subtitle {
+      font-weight: 300;
+      font-size: 42px;
+      color: #526488;
+      word-spacing: 5px;
+      padding-bottom: 15px;
+   }
+
+   .links {
+      padding-top: 15px;
+   }
+}
+</style>
